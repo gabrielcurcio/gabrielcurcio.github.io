a68285fef839b2edb30c3f8a7d6da0b4390f497a
gabrielcurcio <123421010+gabrielcurcio@users.noreply.github.com>
2023-01-23 19:53:07 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+
+function myFunction() {
+  var x = document.getElementById("l1");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction2() {
+  var x = document.getElementById("l2");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function myFunction3() {
+  var x = document.getElementById("l3");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
